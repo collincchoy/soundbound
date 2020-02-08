241e5947044bd6dc1351b5aa9bca58c527dfd56b
Collin Choy <collin@choy.me>
2020-02-08 01:19:12 -0500

    Pimp out the navbar.

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import { useLoginContext } from "../hooks";
+import { useLoginContext } from "../../hooks";
+import sb from "./soundbound.png";
 
-function CurrentUser(props: { pictureUrl: string; name: string, logout: () => void}) {
+function CurrentUser(props: {
+  pictureUrl: string;
+  name: string;
+  logout: () => void;
+}) {
   const { name, pictureUrl, logout } = props;
   const [isClicked, setIsClicked] = useState(false);
   return (
     <div className={`navbar-item has-dropdown ${isClicked ? "is-active" : ""}`}>
       <div className="navbar-link is-arrowless">
-        <button
-          className="button is-white"
+        <div
+          className="button is-dark"
           onClick={() => setIsClicked(!isClicked)}
         >
           <div className="media">
@@ -17,10 +22,12 @@ function CurrentUser(props: { pictureUrl: string; name: string, logout: () => vo
             </figure>
             <h2>{name}</h2>
           </div>
-        </button>
+        </div>
       </div>
       <div className={`navbar-dropdown is-right`}>
-        <a className="navbar-item" onClick={logout}>Logout</a>
+        <a className="navbar-item" onClick={logout}>
+          Logout
+        </a>
       </div>
     </div>
   );
@@ -39,38 +46,29 @@ function LoggedInUser() {
     name: currentUser?.display_name ?? "John Smith",
     pictureUrl: currentUser?.images[0]?.url ?? "#",
     logout
-  }
+  };
   return isLoggedIn ? (
-    <CurrentUser {...currentUserProps}/>
+    <CurrentUser {...currentUserProps} />
   ) : (
     <div className="navbar-item">{logInButton}</div>
   );
 }
 
 export default function NavHeader() {
-  const Logo = (
-    <a className="navbar-item" href="https://bulma.io">
-      <img
-        src="https://bulma.io/images/bulma-logo.png"
-        width="112"
-        height="28"
-      />
-    </a>
-  );
-
   const NavMenuLeft = (
     <div className="navbar-start">
-      <a className="navbar-item">Home</a>
-
-      <a className="navbar-item">Documentation</a>
+      <a className="navbar-item" href="/">
+        Top
+      </a>
+      
+      <a className="navbar-item" href="/">Lab</a>
 
       <div className="navbar-item has-dropdown is-hoverable">
-        <a className="navbar-link">More</a>
+        <a className="navbar-link" href="/">Discover</a>
 
         <div className="navbar-dropdown">
-          <a className="navbar-item">About</a>
-          <a className="navbar-item">Jobs</a>
-          <a className="navbar-item">Contact</a>
+          <a className="navbar-item" href="/">Artists</a>
+          <a className="navbar-item" href="/">Genres</a>
           <hr className="navbar-divider" />
           <a className="navbar-item">Report an issue</a>
         </div>
@@ -89,7 +87,7 @@ export default function NavHeader() {
     return (
       <a
         role="button"
-        className={`navbar-burger burger ${isBurgerActive ? "is-active": ""}`}
+        className={`navbar-burger burger ${isBurgerActive ? "is-active" : ""}`}
         onClick={() => setIsBurgerActive(!isBurgerActive)}
         aria-label="menu"
         aria-expanded="false"
@@ -103,13 +101,18 @@ export default function NavHeader() {
     );
   };
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        {Logo}
+        <a className="navbar-item" href="/">
+          <img src={sb} alt="logo" />
+        </a>
         <NavBurger />
       </div>
-      <div id="navbarBasicExample" className={`navbar-menu ${isBurgerActive ? "is-active": ""}`}>
-        {/* {NavMenuLeft} */}
+      <div
+        id="navbarBasicExample"
+        className={`navbar-menu ${isBurgerActive ? "is-active" : ""}`}
+      >
+        {NavMenuLeft}
         {NavMenuRight}
       </div>
     </nav>
