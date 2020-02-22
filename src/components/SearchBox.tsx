05b5009bcbd982d211c415b926f08f34a77f1dcb
Collin Choy <collin@choy.me>
2020-02-22 17:55:11 -0500

    Add keyboard nav controls to the searchbox suggestions.

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -13,20 +13,22 @@ export default function SearchBox<T>({
   suggestionKey,
   ...props
 }: SearchBoxProps<T>) {
-  const [selectedItem, setSelectedItem] = useState<T>();
   const [isSearching, setIsSearching] = useState(false);
+  const [selectedSuggestionIndex, setSelectedSuggestionIndex] = useState<
+    number
+  >(-1);
   const [suggestions, setSuggestions] = useState<T[]>([]);
   const clearSuggestions = () => {
     setSuggestions([]);
+    setSelectedSuggestionIndex(-1);
   };
 
-  const [field, meta, helpers] = useField(props.name);
+  const [field, , helpers] = useField(props.name);
   const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const input = event.target as HTMLInputElement;
     field.onChange(event);
     if (input.value !== "") {
       let searchResults = getSuggestions(input.value);
-    if (input.value !== "") {
       setIsSearching(true);
       setSuggestions(await searchResults);
     } else {
@@ -37,6 +39,10 @@ export default function SearchBox<T>({
   const handleBlur = async (event: React.FocusEvent) => {
     field.onBlur(event);
     setIsSearching(false);
+    clearSuggestions();
+  };
+  const selectItem = (value: string) => {
+    helpers.setValue(value);
     clearSuggestions();
   };
 
@@ -51,25 +57,58 @@ export default function SearchBox<T>({
         {...props}
         onChange={handleChange}
         onBlur={handleBlur}
+        onKeyDown={(e: React.KeyboardEvent) => {
+          switch (e.key) {
+            case "ArrowUp": {
+              e.preventDefault(); // Moves cursor to start of input box
+              if (selectedSuggestionIndex > -1)
+                setSelectedSuggestionIndex(selectedSuggestionIndex - 1);
+              break;
+            }
+            case "ArrowDown": {
+              // Note: do not preventDefault here b/c moves cursor to end of input box is useful
+              if (selectedSuggestionIndex < suggestions.length)
+                setSelectedSuggestionIndex(selectedSuggestionIndex + 1);
+              break;
+            }
+            case "Enter": {
+              if (
+                -1 < selectedSuggestionIndex &&
+                selectedSuggestionIndex < suggestions.length
+              ) {
+                e.preventDefault(); // Do not submit form
+                const { value } = suggestionKey(
+                  suggestions[selectedSuggestionIndex]
+                );
+                selectItem(value);
+              }
+              break;
+            }
+            default: {
+            }
+          }
+        }}
         autoComplete="off"
       />
       {suggestions.length > 0 ? (
-        <ol className={styles.suggestions}>
-          {suggestions.map((item: any) => {
+        <ol
+          className={styles.suggestions}
+          onMouseOut={() => setSelectedSuggestionIndex(-1)}
+        >
+          {suggestions.map((item: any, index: number) => {
             const { key, value } = suggestionKey(item);
             return (
               <li
-                className={styles.suggestion}
+                className={`${styles.suggestion} ${
+                  index === selectedSuggestionIndex ? styles.hover : ""
+                }`}
                 key={key}
-                onClick={() => {
-                  console.log("hey - ", value);
-                  helpers.setValue(value);
-                  clearSuggestions();
-                }}
+                onClick={() => selectItem(value)}
                 onMouseDown={
                   /*Prevent blur event to avoid race condition with click event.*/
                   e => e.preventDefault()
                 }
+                onMouseOver={() => setSelectedSuggestionIndex(index)}
               >
                 {value}
               </li>
