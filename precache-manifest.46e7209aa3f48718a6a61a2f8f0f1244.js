eac4bf0ca39d350a27a9396ebdd249d6a5d38d3d
Collin Choy <collin@choy.me>
2020-03-14 19:15:23 -0400

    Updates

---
+++ precache-manifest.46e7209aa3f48718a6a61a2f8f0f1244.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009bebbb6ba260f50e0e3bbf40bb3719",
+    "url": "/soundbound/index.html"
+  },
+  {
+    "revision": "605873d483c5145a7610",
+    "url": "/soundbound/static/css/main.45ef332c.chunk.css"
+  },
+  {
+    "revision": "5b4ddc8b9fcabe3ffbfb",
+    "url": "/soundbound/static/js/2.c5303645.chunk.js"
+  },
+  {
+    "revision": "605873d483c5145a7610",
+    "url": "/soundbound/static/js/main.8403bb69.chunk.js"
+  },
+  {
+    "revision": "57b59ecc2e34796a2d00",
+    "url": "/soundbound/static/js/runtime-main.91f0f319.js"
+  },
+  {
+    "revision": "47325efb3a912a33f6d3370e6dcced8c",
+    "url": "/soundbound/static/media/Bubbles.47325efb.svg"
+  },
+  {
+    "revision": "a0b13cf1697b9f13b439f5c463a8609b",
+    "url": "/soundbound/static/media/MixingPot.a0b13cf1.svg"
+  },
+  {
+    "revision": "107833986317315db974c39ce1e868aa",
+    "url": "/soundbound/static/media/soundbound.10783398.png"
+  }
+]);
