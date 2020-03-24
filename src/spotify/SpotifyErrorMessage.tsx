6633c76ed89925758c1279af6a7b614a083ce138
Collin Choy <collin@choy.me>
2020-03-23 23:03:44 -0400

    Make the spotify error message comp snazzy.

--- src/spotify/SpotifyErrorMessage.tsx
+++ src/spotify/SpotifyErrorMessage.tsx
@@ -2,23 +2,30 @@ import React from "react";
 
 import { spotify } from "./api";
 
-export default function SpotifyErrorMessage(props: {
-  status: number;
-  message: string;
-}) {
-  const { status, message } = props;
+const SpotifyErrorMessage: React.FC<{ status: number; message: string }> = ({
+  status,
+  message
+}) => {
   return (
-    <div className="container">
-      {status === 401 ? (
-        <span>
-          Oops you need to authorize this app&nbsp;
-          <a href={spotify.authorizeUrl.toString()}>Click here to authorize.</a>
-        </span>
-      ) : (
-        <span>
-          Uh oh. An Error Occurred: {status} {message}
-        </span>
-      )}
+    <div className="container message is-danger">
+      <div className="message-header">
+        <p>Error</p>
+        <button className="delete" aria-label="delete"></button>
+      </div>
+      <div className="message-body">
+        {status === 401 ? (
+          <span>
+            Oops you need to&nbsp;
+            <a href={spotify.authorizeUrl.toString()}>reauthorize this app.</a>
+          </span>
+        ) : (
+          <span>
+            Uh oh. An Error Occurred: {status} {message}
+          </span>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default SpotifyErrorMessage;
