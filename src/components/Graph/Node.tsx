7589b9e2ffb2047b57c8195dfb12a9baefb9f111
Collin Choy <collin@choy.me>
2022-06-04 00:18:01 -0400

    Add color/fill to Node.

--- src/components/Graph/Node.tsx
+++ src/components/Graph/Node.tsx
@@ -2,12 +2,19 @@ import React from "react";
 
 interface Props {
   size?: number;
+  fill?: string;
 }
 
-const Node = ({ size = 150 }: Props) => {
+const Node = ({ size = 150, fill = "#C4C4C4" }: Props) => {
   return (
     <svg width={size} height={size}>
-      <ellipse cx={size / 2} cy={size / 2} rx={size / 2} ry={size / 2} />
+      <ellipse
+        cx={size / 2}
+        cy={size / 2}
+        rx={size / 2}
+        ry={size / 2}
+        fill={fill}
+      />
       <line stroke="white" />
     </svg>
   );
