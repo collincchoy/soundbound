b7ad98755fcaa18f3e7df0c07c8c6c870fb130ed
Collin Choy <collin@choy.me>
2022-06-04 04:00:02 -0400

    Add support for images in graph nodes.

--- src/components/Graph/Node.tsx
+++ src/components/Graph/Node.tsx
@@ -3,23 +3,38 @@ import styled from "styled-components";
 
 interface Props {
   size?: number;
-  fill?: string;
+  color?: string;
+  imageUrl?: string;
 }
 
 const StyledSvg = styled.svg<Props>`
   z-index: 200;
 `;
 
-const Node = ({ size = 150, fill = "#C4C4C4" }: Props) => {
+const Node = ({ size = 150, color = "#C4C4C4", imageUrl }: Props) => {
+  const fill = imageUrl ? "url(#image)" : color;
   return (
     <StyledSvg width={size} height={size}>
-      <ellipse
-        cx={size / 2}
-        cy={size / 2}
-        rx={size / 2}
-        ry={size / 2}
-        fill={fill}
-      />
+      {imageUrl && (
+        <defs>
+          <pattern
+            id="image"
+            patternUnits="userSpaceOnUse"
+            height={size}
+            width={size}
+          >
+            <image
+              x="-25%"
+              y="-25%"
+              height={size * 1.5}
+              width={size * 1.5}
+              xlinkHref={imageUrl}
+            />
+          </pattern>
+        </defs>
+      )}
+
+      <circle cx={size / 2} cy={size / 2} r={size / 2} fill={fill} />
     </StyledSvg>
   );
 };
