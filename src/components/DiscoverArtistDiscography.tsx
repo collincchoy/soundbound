d5d50683c008d7735f4ce194570c244c8060753c
Collin Choy <collin@choy.me>
2022-12-04 23:34:49 -0500

    Add popovers to albums on discover page.

--- src/components/DiscoverArtistDiscography.tsx
+++ src/components/DiscoverArtistDiscography.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from "react";
 import { usePaginatedSpotifyApi } from "spotify/hooks";
 import { Album, AlbumType, Artist } from "spotify/types";
+import styled from "styled-components";
 
 export default function DiscoverArtistDiscography({
   artist,
@@ -23,11 +24,12 @@ export default function DiscoverArtistDiscography({
 
   const renderAlbumImages = (albums: Album[]) => {
     return albums.map((album) => (
-      <img
+      <HoverableAlbum
         src={album?.images[album?.images?.length - 1]?.url}
         alt="Album Cover"
         title={album.name}
         key={album.id}
+        popoverMessage={album.name}
       />
     ));
   };
@@ -46,3 +48,54 @@ export default function DiscoverArtistDiscography({
     </div>
   );
 }
+
+const HoverableAlbum: React.FC<{
+  src: string;
+  alt: string;
+  title: string;
+  popoverMessage: string;
+}> = (props) => {
+  return (
+    <StyledAlbumCover data-popover-message={props.popoverMessage}>
+      <img
+        src={props.src}
+        title={props.title}
+        alt={props.alt}
+        className="image is-64x64"
+      />
+    </StyledAlbumCover>
+  );
+};
+
+const StyledAlbumCover = styled.div`
+  display: inline-block;
+  cursor: pointer;
+  position: relative;
+
+  &::after {
+    // visibility
+    display: none;
+
+    // content
+    content: attr(data-popover-message);
+    width: max-content;
+    background-color: black;
+    border-radius: 5px;
+    border: 1px solid white;
+    padding: 0.5em;
+
+    // positioning
+    position: absolute;
+    z-index: 100;
+    top: 0;
+    left: 50%;
+    translate: -50% -100%;
+  }
+
+  &:hover {
+    &::after {
+      // visibility
+      display: block;
+    }
+  }
+`;
